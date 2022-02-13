8678a1bfbb2a3c8eb79c44396af512a05c5aa2f6
Mojakun <kazuya.6289@gmail.com>
2022-02-13 22:46:32 +0900

    いらないファイルの削除

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
-const reactPath = path.resolve(__dirname, 'src');
+const srcPath = path.resolve(__dirname, 'src');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
-      '@entities': path.resolve(reactPath, 'entities'),
-      '@hooks': path.resolve(reactPath, 'hooks'),
-      '@models': path.resolve(reactPath, 'models'),
-      '@services': path.resolve(reactPath, 'services'),
-      '@atoms': path.resolve(reactPath, 'atomic/atoms'),
-      '@molecules': path.resolve(reactPath, 'atomic/molecules'),
-      '@organisms': path.resolve(reactPath, 'atomic/organisms'),
-      '@pages': path.resolve(reactPath, 'atomic/pages'),
+      '@entities': path.resolve(srcPath, 'entities'),
+      '@hooks': path.resolve(srcPath, 'hooks'),
+      '@models': path.resolve(srcPath, 'models'),
+      '@services': path.resolve(srcPath, 'services'),
+      '@atoms': path.resolve(srcPath, 'atomic/atoms'),
+      '@molecules': path.resolve(srcPath, 'atomic/molecules'),
+      '@organisms': path.resolve(srcPath, 'atomic/organisms'),
+      '@pages': path.resolve(srcPath, 'atomic/pages'),
     },
   },
 };
