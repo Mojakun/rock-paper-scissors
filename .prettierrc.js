a68f901fdf7d946fa00e7df758b3125ef2d51c75
Mojakun <kazuya.6289@gmail.com>
2022-02-13 23:21:32 +0900

    test

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,10 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 100,
+  printWidth: 120,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false
 };
