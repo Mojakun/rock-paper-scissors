9217d68a1f553dd6e397dc77992e31f76468909e
Mojakun <kazuya.6289@gmail.com>
2022-02-18 06:45:39 +0900

    crate_models

---
+++ src/models/GameModel.js
@@ -0,0 +1,10 @@
+class GameModel {
+  static isWin(userValue, enemyValue) {
+    return;
+  }
+  static isEven(userValue, enemyValue) {
+    return userValue == enemyValue;
+  }
+  static isLose(userValue, enemyValue) {}
+}
+export default GameModel;
