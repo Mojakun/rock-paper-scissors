e52587c7f795ce0797d5a93e68be1abfff58e77a
Mojakun <kazuya.6289@gmail.com>
2022-02-18 05:19:55 +0900

    tailwind_setup_done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#C43BAD",
+        secondary: "#46C9E5",
+      },
+    },
+  },
+  plugins: [],
+}
