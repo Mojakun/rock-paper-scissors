2afa6c8b675b6a8c55892726e5ae5c0599ca9b02
Mojakun <kazuya.6289@gmail.com>
2022-02-18 08:01:17 +0900

    ロジック実装

--- src/models/RockPaerScissorsModel.js
+++ src/models/RockPaerScissorsModel.js
@@ -1,3 +1,4 @@
+import GameModel from './GameModel';
 class RockPaerScissorsModel {
   static ROCK = 1;
   static PAPER = 2;
@@ -11,6 +12,21 @@ class RockPaerScissorsModel {
   }
   static isScissorsWinnerHand() {
     return this.SCISSORS;
+  }
+  static judgementByRock(value) {
+    if (this.isRockWinnerHand === value) return GameModel.WIN;
+    if (this.ROCK === value) return GameModel.EVEN;
+    return GameModel.LOSE;
+  }
+  static judgementByPaper(value) {
+    if (this.isPaperWinnerHand == value) return GameModel.WIN;
+    if (this.PAPER === value) return GameModel.EVEN;
+    return GameModel.LOSE;
+  }
+  static judgementByScissors(value) {
+    if (this.isScissorsWinnerHand == value) return GameModel.WIN;
+    if (this.SCISSORS === value) return GameModel.EVEN;
+    return GameModel.LOSE;
   }
 }
 
