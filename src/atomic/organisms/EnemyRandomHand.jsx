b31abe4bf057f6b3f9524c40c39e61ecd5da677b
Mojakun <kazuya.6289@gmail.com>
2022-02-18 06:43:56 +0900

    必要そうなコンポーネント作成

---
+++ src/atomic/organisms/EnemyRandomHand.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RockPaerScissorsModel from '@models/RockPaerScissorsModel';
+const EnemyRandomHand = ({ value }) => {
+  if (value == RockPaerScissorsModel.ROCK) {
+    return <div>ROCK</div>;
+  }
+  if (value == RockPaerScissorsModel.PAER) {
+    return <div>PAER</div>;
+  }
+  if (value == RockPaerScissorsModel.SCISSORS) {
+    return <div>SCISSORS</div>;
+  }
+};
+
+export default EnemyRandomHand;
--- src/atomic/pages/Home.jsx
+++ src/atomic/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import Container from '@templates/Container';
+import EnemyRandomHand from '@organisms/EnemyRandomHand';
+import UserScissorsHand from '@organisms/UserScissorsHand';
+import UserRockHand from '@organisms/UserRockHand';
+import UserPaperHand from '@organisms/UserPaperHand';
+const Home = () => {
+  const [enemyValue, setEnemyValue] = useState(1);
 
-function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+  return (
+    <Container>
+      <div>
+        <EnemyRandomHand value={enemyValue} />
+      </div>
+      <div>
+        <UserRockHand />
+        <UserPaperHand />
+        <UserScissorsHand />
+      </div>
+    </Container>
+  );
+};
 
 export default Home;
