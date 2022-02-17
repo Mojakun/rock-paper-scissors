46d76966e216b48de84277dcc4dbc791517a3465
Mojakun <kazuya.6289@gmail.com>
2022-02-18 08:09:55 +0900

    ロジック修正

--- src/models/RockPaerScissorsModel.js
+++ src/models/RockPaerScissorsModel.js
@@ -4,27 +4,32 @@ class RockPaerScissorsModel {
   static PAPER = 2;
   static SCISSORS = 3;
 
-  static isRockWinnerHand() {
-    return this.PAPER;
+  static ROCK_WINNER_HAND = this.PAPER;
+  static PAPER_WINNER_HAND = this.SCISSORS;
+  static SCISSORS__WINNER_HAND = this.ROCK;
+
+  static isRockWinnerHand(value) {
+    return this.ROCK_WINNER_HAND === value;
   }
-  static isPaperWinnerHand() {
-    return this.SCISSORS;
+  static isPaperWinnerHand(value) {
+    return this.PAPER_WINNER_HAND === value;
   }
-  static isScissorsWinnerHand() {
-    return this.SCISSORS;
+  static isScissorsWinnerHand(value) {
+    return this.SCISSORS__WINNER_HAND === value;
   }
+
   static judgementByRock(value) {
-    if (this.isRockWinnerHand === value) return GameModel.WIN;
+    if (this.isRockWinnerHand(value)) return GameModel.WIN;
     if (this.ROCK === value) return GameModel.EVEN;
     return GameModel.LOSE;
   }
   static judgementByPaper(value) {
-    if (this.isPaperWinnerHand == value) return GameModel.WIN;
+    if (this.isPaperWinnerHand(value)) return GameModel.WIN;
     if (this.PAPER === value) return GameModel.EVEN;
     return GameModel.LOSE;
   }
   static judgementByScissors(value) {
-    if (this.isScissorsWinnerHand == value) return GameModel.WIN;
+    if (this.isScissorsWinnerHand(value)) return GameModel.WIN;
     if (this.SCISSORS === value) return GameModel.EVEN;
     return GameModel.LOSE;
   }
